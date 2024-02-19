bb1a01e9eebb5511f17ffc8a34a12aa3d02c94bf
Trong-Nguyen-DT <trongnguyen.2002.nguyenvan@gmail.com>
2024-02-19 16:45:05 +0700

    feat: Implement getAllBlogsByBlogStatus API for viewing blog list by status
    - Created getAllBlogsByBlogStatus API endpoint in the Admin section to retrieve blog list based on status.
    - Added validation to check the validity of the status parameter.
    - Handled cases where the status is invalid or the page number is not found.
    - Returned appropriate response codes and messages for different scenarios.
    - Integrated with existing service methods to fetch and paginate blog data.

--- BackEnd/src/main/java/com/dac/BackEnd/controller/Admin/BlogsAdminController.java
+++ BackEnd/src/main/java/com/dac/BackEnd/controller/Admin/BlogsAdminController.java
@@ -4,16 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Arrays;
+import java.util.ArrayList;
 
+import com.dac.BackEnd.entity.BlogEntity.BlogStatus;
 import com.dac.BackEnd.exception.MessageException;
+import com.dac.BackEnd.model.Blog;
 import com.dac.BackEnd.model.response.ResponseBody;
 import com.dac.BackEnd.model.response.ResponsePage;
 import com.dac.BackEnd.service.BlogService;
+import com.dac.BackEnd.validation.BlogStatusValidation;
 
 
 @RestController
@@ -32,6 +38,27 @@ public class BlogsAdminController {
             body.setData(blogService.getAllBlogs(page));
             body.setPageInfo(responsePage);
             body.setMessage(Arrays.asList("Success"));
+            return ResponseEntity.ok().body(body);
+        } catch (Exception e) {
+            ResponseBody body = new ResponseBody();
+            body.setCode(404);
+            body.setMessage(Arrays.asList(new MessageException(e.getMessage(), 404)));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+        }
+    }
+
+    @GetMapping("filter")
+    public ResponseEntity<ResponseBody> getAllBlogsByBlogStatus(@RequestParam(required = false, defaultValue = "1") int page, @RequestParam(required = false, defaultValue = "WAITING") String status) {
+        try {
+            BlogStatus blogStatus = BlogStatusValidation.checkValidStatus(status);
+            ResponsePage responsePage = blogService.getPageInfoByStatus(page, blogStatus);
+            ResponseBody body = new ResponseBody();
+
+            body.setCode(200);
+            body.setData(blogService.getAllBlogsByStatus(page, blogStatus));
+            body.setMessage(Arrays.asList("Success"));
+            body.setPageInfo(responsePage);
+
             return ResponseEntity.ok().body(body);
         } catch (Exception e) {
             ResponseBody body = new ResponseBody();
