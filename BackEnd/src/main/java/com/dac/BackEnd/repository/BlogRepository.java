90bf4859e7cd7410538402d4de5caa96104a6eb1
Trong-Nguyen-DT <trongnguyen.2002.nguyenvan@gmail.com>
2024-02-20 17:09:19 +0700

    Fix: Refactor API for getAllBlog functionality
    Refactored the getAllBlog API in the ADMIN module to handle search filters and pagination correctly. Updated controller, service, and repository layers accordingly.

--- BackEnd/src/main/java/com/dac/BackEnd/repository/BlogRepository.java
+++ BackEnd/src/main/java/com/dac/BackEnd/repository/BlogRepository.java
@@ -1,5 +1,6 @@
 package com.dac.BackEnd.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.dac.BackEnd.entity.BlogEntity.BlogEntity;
 import com.dac.BackEnd.entity.BlogEntity.BlogStatus;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -16,12 +18,22 @@ public interface BlogRepository extends JpaRepository<BlogEntity, Long>{
     @Query(value = "SELECT COUNT(*) FROM blogs", nativeQuery = true)
     int countAllBlogs();
 
-    @Query(value = "SELECT * FROM blogs ORDER BY insert_date_time DESC LIMIT 10 OFFSET :pageNumber", nativeQuery = true)
-    List<BlogEntity> findAllBlogsPerPage(@Param("pageNumber") int pageNumber);
-
     @Query(value = "SELECT COUNT(*) FROM blogs WHERE status = :status", nativeQuery = true)
     int countAllBlogsByStatus(@Param("status") BlogStatus status);
 
-    @Query(value = "SELECT * FROM blogs WHERE status = :status ORDER BY insert_date_time DESC LIMIT 10 OFFSET :pageNumber", nativeQuery = true)
-    List<BlogEntity> findAllByStatus(@Param("status") BlogStatus status, @Param("pageNumber") int pageNumber);
+    @Query("SELECT COUNT(b) FROM BlogEntity b JOIN b.film f JOIN b.insertBy r WHERE b.title LIKE %:searchTerm% OR f.nameFilm LIKE %:searchTerm% OR r.name LIKE %:searchTerm%")
+    int countAllBlogsByText(String searchTerm);
+
+    @Query("SELECT COUNT(b) FROM BlogEntity b WHERE b.postTime BETWEEN :startTime AND :endTime")
+    int countAllBlogsByPostTime(@Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
+
+    List<BlogEntity> findAllByOrderByInsertDateTimeDesc(Pageable pageable);
+
+    List<BlogEntity> findAllByStatusOrderByInsertDateTimeDesc(BlogStatus status, Pageable pageable);
+
+    @Query("SELECT b FROM BlogEntity b JOIN b.film f JOIN b.insertBy r WHERE b.title LIKE %:searchTerm% OR f.nameFilm LIKE %:searchTerm% OR r.name LIKE %:searchTerm% ORDER BY b.insertDateTime DESC")
+    List<BlogEntity> findAllBySearchText(@Param("searchTerm") String searchTerm, Pageable pageable);
+
+    List<BlogEntity> findAllByPostTimeBetween(LocalDateTime startTime, LocalDateTime endTime, Pageable pageable);
+
 }
