a31c8b305c3579784bf37a115d97ff76c3fed9df
Trong-Nguyen-DT <trongnguyen.2002.nguyenvan@gmail.com>
2024-03-06 13:05:19 +0700

    feat: Authen login

--- FrontEnd/frontend/src/index.js
+++ FrontEnd/frontend/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Routes } from"react-router-dom";
 
 import SignUp from "./pages/SignUp/SignUp";
 import Login from "./pages/Login/Login";
-import UserPage from "./pages/User/DashboardPage";
+import DashboardPage from "./pages/User/DashboardPage";
 import ReviewerPage from "./pages/User/ReviewerPage";
 import FilmPage from "./pages/User/FilmPage";
 import BlogPage from "./pages/User/BlogPage";
@@ -16,15 +16,15 @@ import FilmsPage from "./pages/Guest/FilmsPage";
 import BlogByFilm from "./components/blogGuest/BlogByFilm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
+
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
+      <Routes >
         <Route index element={<App />} />
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<SignUp />} />
-        <Route path="/user" element={<UserPage />} />
+        <Route path="/user" element={<DashboardPage />} />
         <Route path="/user/reviewers" element={<ReviewerPage />} />
         <Route path="/user/films" element={<FilmPage />} />
         <Route path="/user/blogs" element={<BlogPage />} />
